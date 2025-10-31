af88d5b305020dd450da462ff703527071314f56
pankajk <pankaj.160907@gmail.com>
2025-10-31 14:02:14 +1100

    removed subnets for database.

--- variables.tf
+++ variables.tf
@@ -1,6 +1,5 @@
 variable "domain_name" {
-  description = "Name of the domain I bought from AWS"
+  description = "Name of the domain bought from AWS" # It makes life a little simpler if using Route53.
   type        = string
   default     = "uselesschatter.com"
 }
-
