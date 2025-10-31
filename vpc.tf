cd796dbc0aac85cfbd918ea0c27a982b6c586bbd
pankajk <pankaj.160907@gmail.com>
2025-10-31 14:02:34 +1100

    removed error

--- vpc.tf
+++ vpc.tf
@@ -7,7 +7,7 @@ module "vpc" {
   azs              = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets  = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
   # database_subnets = ["10.0.20.0/24", "10.0.21.0/24", "10.0.22.0/24"]
-  # public_subnets   = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets   = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
 
   # EC2 instances in private subnet need internet access. Like OS updates.
   # Outside world cannot initiate connection to instances in private subnet.
